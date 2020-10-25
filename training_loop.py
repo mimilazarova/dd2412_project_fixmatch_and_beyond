b69fb3e08967fbef256a920a6785e17068a21f1b
Johan Broberg <johan.broberg92@gmail.com>
2020-10-25 21:26:44 +0100

    lr schedule, adoptation to our data and simple logging

--- training_loop.py
+++ training_loop.py
@@ -1,10 +1,3 @@
-
-#import pip
-#import sys
-#import subprocess
-#!pip install tensorflow-addons==0.11.1
-#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tensorflow-addons==0.11.1'])
-
 import os
 import logging
 
@@ -13,23 +6,46 @@ logging.getLogger('tensorflow').disabled = True
 
 import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds
 import tensorflow_addons as tfa
 from tqdm import tqdm, tqdm_notebook
-
 from augment import CTAugment
 
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+import math
 
-def training(model, ds, mean=None, std=None, lr_values=[0.01, 0.1, 0.01, 0.001],
-             lr_boundaries=[400, 32000, 48000, 64000],
-             val_interval=2000, log_interval=200, batch_size=128, nesterov=False):
-    schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(boundaries=lr_boundaries[:-1], values=lr_values)
-    optimizer = tf.keras.optimizers.SGD(schedule, momentum=0.9, nesterov=nesterov)
+
+class OurCosineDecay(tf.keras.experimental.CosineDecay):
+
+    def __call__(self, step):
+        with ops.name_scope_v2(self.name or "CosineDecay"):
+            initial_learning_rate = ops.convert_to_tensor_v2(
+                self.initial_learning_rate, name="initial_learning_rate")
+            dtype = initial_learning_rate.dtype
+            decay_steps = math_ops.cast(self.decay_steps, dtype)
+
+            global_step_recomp = math_ops.cast(step, dtype)
+            global_step_recomp = math_ops.minimum(global_step_recomp, decay_steps)
+            completed_fraction = global_step_recomp / decay_steps
+            cosine_decayed = math_ops.cos(
+                constant_op.constant(7 / 16 * math.pi) * completed_fraction)
+
+            decayed = (1 - self.alpha) * cosine_decayed + self.alpha
+            return math_ops.multiply(initial_learning_rate, decayed)
+
+
+def training(model, ds_l, ds_u, hparams, mean=None, std=None,
+             val_interval=2000, log_interval=200, batch_size=128):
+    
+    schedule = OurCosineDecay(hparams['eta'], hparams['K'])
+    optimizer = tf.keras.optimizers.SGD(schedule, momentum=hparams['beta'], nesterov=hparams['nesterov'])
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    cta = CTAugment(hparams['cta_classes'], hparams['cta_decay'], hparams['cta_threshold'], hparams['cta_depth'])
 
     def train_prep(x, y):
         x = tf.cast(x, tf.float32) / 255.
-        # x = tf.image.random_flip_left_right(x)
         # x = tf.image.pad_to_bounding_box(x, 4, 4, 40, 40) #must fix ds independent shape
         # x = tf.image.random_crop(x, (32, 32, 3))          #must fix ds independent shape
         if mean is not None and std is not None:
@@ -43,8 +59,10 @@ def training(model, ds, mean=None, std=None, lr_values=[0.01, 0.1, 0.01, 0.001],
         return x, y
 
     # ds['train'] = ds['train'].map(train_prep).shuffle(10000).repeat().batch(batch_size).prefetch(-1)
-    ds['train'] = ds['train'].map(train_prep).batch(batch_size).prefetch(-1)
-    ds['test'] = ds['test'].map(valid_prep).batch(batch_size * 4).prefetch(-1)
+    ds_l['train'] = ds_l['train'].map(train_prep).batch(hparams['B']).prefetch(-1)
+    ds_u['train'] = ds_u['train'].map(train_prep).batch(hparams['mu'] * hparams['B']).prefetch(-1)
+
+    ds_l['test'] = ds_l['test'].map(valid_prep).batch(batch_size * 4).prefetch(-1)
 
     # runid = run_name + '_x' + str(np.random.randint(10000))
     # writer = tf.summary.create_file_writer(logdir + '/' + runid)
@@ -61,10 +79,6 @@ def training(model, ds, mean=None, std=None, lr_values=[0.01, 0.1, 0.01, 0.001],
         shift = tf.random.uniform([x.shape[0], 2], minval=-max_shift, maxval=max_shift, dtype=tf.dtypes.int32)
         return tfa.image.translate(x, tf.cast(shift, tf.dtypes.float32))
 
-    def strong_transformation(x):
-        cta = CTAugment(10, 0.99, 0.8, 2, 10)
-        return cta.augment(x)
-
     def pseudolabel(class_dist):
         argmax = tf.math.argmax(class_dist, axis=1)
         return tf.one_hot(argmax, class_dist.shape[1])
@@ -80,32 +94,25 @@ def training(model, ds, mean=None, std=None, lr_values=[0.01, 0.1, 0.01, 0.001],
             x_wk = weak_transformation(x)
             outs_wk = model(x_wk, training)  # should this be training or not?
             weak_labels = pseudolabel(outs_wk)
-            weak_labels = threshold_gate(weak_labels, outs_wk, 0.7)
-
-            cta = CTAugment(10, 0.99, 0.8, 2)
-
-            # x_str = cta.augment(x)
-            # x_str = cta.augment_batch(x)
+            weak_labels = threshold_gate(weak_labels, outs_wk, hparams['treshold'])
 
             x_str, choices, bins = cta.augment_batch(x)
-
             outs_str = model(x_str, training)
-
             cta.update_weights_batch(weak_labels, outs_str, choices, bins)
 
-            # unlabeled_loss = loss_fn(weak_labels, outs_str)
+            unlabeled_loss = loss_fn(weak_labels, outs_str)
 
             # labeled data
-            # outs = model(x, training)
-            # labeled_loss = loss_fn(y, outs)
+            outs = model(x, training)
+            labeled_loss = loss_fn(y, outs)
 
             # add losses together
-            # loss = labeled_loss + lambda * unlabeled_loss
+            loss = labeled_loss + hparams['lamda'] * unlabeled_loss
 
-            r_loss = tf.add_n(model.losses)
-            outs = model(x, training)
-            c_loss = loss_fn(y, outs)
-            loss = c_loss + r_loss
+            # r_loss = tf.add_n(model.losses)
+            # outs = model(x, training)
+            # c_loss = loss_fn(y, outs)
+            # loss = c_loss + r_loss
 
         if training:
             gradients = tape.gradient(loss, model.trainable_weights)
@@ -152,58 +159,3 @@ def training(model, ds, mean=None, std=None, lr_values=[0.01, 0.1, 0.01, 0.001],
 
         cls_loss.reset_states()
         accuracy.reset_states()
-
-
-def cifar_error_test(model, tr_len=20, vd_len=2):
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-    optimizer = tf.keras.optimizers.SGD(0.01)
-
-    ds = tfds.load('cifar10', as_supervised=True, in_memory=True)
-    std = tf.reshape((0.2023, 0.1994, 0.2010), shape=(1, 1, 3))
-    mean = tf.reshape((0.4914, 0.4822, 0.4465), shape=(1, 1, 3))
-
-    def train_prep(x, y):
-        x = tf.cast(x, tf.float32) / 255.
-        x = tf.image.random_flip_left_right(x)
-        x = tf.image.pad_to_bounding_box(x, 4, 4, 40, 40)
-        x = tf.image.random_crop(x, (32, 32, 3))
-        x = (x - mean) / std
-        return x, y
-
-    def valid_prep(x, y):
-        x = tf.cast(x, tf.float32) / 255.
-        x = (x - mean) / std
-        return x, y
-
-    ds['train'] = ds['train'].map(train_prep).batch(5).take(tr_len).prefetch(-1)
-    ds['test'] = ds['test'].map(valid_prep).batch(5).take(vd_len).prefetch(-1)
-
-    accuracy = tf.metrics.SparseCategoricalAccuracy()
-    cls_loss = tf.metrics.Mean()
-    reg_loss = tf.metrics.Mean()
-
-    # @tf.function
-    def step(x, y, training):
-        with tf.GradientTape() as tape:
-            r_loss = tf.add_n(model.losses)
-            outs = model(x, training)
-            c_loss = loss_fn(y, outs)
-            loss = c_loss + r_loss
-
-        if training:
-            gradients = tape.gradient(loss, model.trainable_weights)
-            optimizer.apply_gradients(zip(gradients, model.trainable_weights))
-
-        accuracy(y, outs)
-        cls_loss(c_loss)
-        reg_loss(r_loss)
-
-    training_step = 0
-    for x, y in tqdm(ds['train'], desc=f'test', total=tr_len, ncols=100, ascii=True):
-        training_step += 1
-        step(x, y, training=True)
-        c_loss, r_loss, err = cls_loss.result(), reg_loss.result(), 1 - accuracy.result()
-        print(f" c_loss: {c_loss:^6.3f} | r_loss: {r_loss:^6.3f} | err: {err:^6.3f}", end='\r')
-
-    for x, y in ds['test']:
-        step(x, y, training=False)
